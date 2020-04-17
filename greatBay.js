7875aca02da6873dd1f0d529c63eafc1059b6fed
KhanhHoang <Khanhsingapore@gmail.com>
2020-04-17 12:39:24 -0500

    prompt for user choices

--- greatBay.js
+++ greatBay.js
@@ -5,11 +5,38 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "khanh1988",
     database: "greatbay_db"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
     // first function call here
+    prompuser()
 });
+function prompuser() {
+   
+    return inquirer
+        .prompt({
+            type: "list",
+            message: "what would you like to do?",
+            name: "choices",
+            choices: [
+                "list",
+                "bid",
+                "view" ]           
+        }),
+        function(err, res) {
+            if (err) throw err;
+            console.log(res);
+            
+            afterConnection();
+        };
+}
+function afterConnection() {
+    connection.query("SELECT * FROM items", function(err, res) {
+      if (err) throw err;
+      console.table(res);
+      connection.end();
+    });
+  }
