421ea4624a84f30b58751acdb8c19b8f34e8f1d4
Michael Sanderson <jawm17@gmail.com>
2020-04-17 11:51:36 -0500

    initialized files

---
+++ greatbayDB.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS greatbay_db;
+
+CREATE DATABASE greatbay_db;
+
+USE greatbay_db;
+
+CREATE TABLE items (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  category VARCHAR(50) NOT NULL,
+  startingBid INT NOT NULL
+);
